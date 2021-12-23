d0bdd2110fc04215019a3d48f91d03b591c722e4
Mustaq Sait <zainabbai1997@gmail.com>
2021-12-23 22:37:17 +0530

    initial commit

---
+++ test.sql
@@ -0,0 +1,5 @@
+create database test;
+use test;
+
+create table register(id int primary key auto_increment,name varchar(255),password varchar(255),email varchar(255),sex varchar(255),country varchar(255));
+select * from register;
